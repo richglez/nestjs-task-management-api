39b67951e0e4f2ceb380e868bbd991e5c2dd3046
rich <rickygb082002@gmail.com>
2026-04-11 18:39:13 -0600

    feat: implement CreateTaskDto and update task service methods for better task management

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,22 @@
-export class CreateTaskDto {}
+import { Priority, Status } from '@prisma/client';
+import { IsString, IsOptional, IsEnum } from 'class-validator';
+
+export class CreateTaskDto {
+    @IsString()
+    title: string;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsEnum(Status)
+    @IsOptional()
+    status?: Status;
+
+    @IsEnum(Priority)
+    @IsOptional()
+    priority?: Priority;
+
+    @IsString()
+    userId: string;
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { PrismaService } from '../prisma/prisma.service';
@@ -8,22 +8,43 @@ export class TasksService {
     constructor(private prisma: PrismaService) {}
 
     create(createTaskDto: CreateTaskDto) {
-        return 'This action adds a new task';
+        return this.prisma.task.create({ data: createTaskDto });
     }
 
     findAll() {
-        return `This action returns all tasks`;
+        return this.prisma.task.findMany({
+            where: { deletedAt: null }, // traeme todas las tareas que no se han borrado donde deletedAt no se le halla aplicado
+        });
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} task`;
+    async findOne(id: string) {
+        // Validar existencias por un id y los que tienen no como estado deleted
+        const task = await this.prisma.task.findFirst({
+            where: { id, deletedAt: null }, // Dame el primer task con este id que NO esté eliminado
+        });
+
+        if (!task) throw new NotFoundException(`Task ${id} not found`);
+
+        return task;
     }
 
-    update(id: number, updateTaskDto: UpdateTaskDto) {
-        return `This action updates a #${id} task`;
+    async update(id: string, updateTaskDto: UpdateTaskDto) {
+        await this.findOne(id); // valida que exista y no esté soft-deleted
+
+        return this.prisma.task.update({
+            where: { id },
+            data: updateTaskDto,
+        });
     }
 
-    remove(id: number) {
-        return `This action removes a #${id} task`;
+    async remove(id: string) {
+        // Validar existencia
+        await this.findOne(id); // Funcion para ver si existe el id
+
+        // soft delete — no borra el registro, solo marca deletedAt
+        return this.prisma.task.update({
+            where: { id },
+            data: { deletedAt: new Date() }, // actualizar nadamas el estado de eliminado
+        });
     }
 }
